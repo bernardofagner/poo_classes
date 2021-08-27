dbd55f08df57b85afc9f60d667ae66037d1c9d16
Grazielle Amanda do Carmo Café <grazielle.cafe@dtidigital.com.br>
2021-08-27 18:11:28 -0300

    Commit do fork

--- Aula 2 - Classes, Propriedades, Instancias/SerHumano/Pessoa.js
+++ Aula 2 - Classes, Propriedades, Instancias/SerHumano/Pessoa.js
@@ -8,8 +8,8 @@ var Pessoa = /** @class */ (function () {
         return new Pessoa(nome, idade);
     };
     /*Metodo possui 2 comportamentos padrão
-    1 - Realizar uma opração e retornar o resultado;
-    2 - Realizar uma operaçaõ sem retornar resultado; */
+    1 - Realizar uma operação e retornar o resultado;
+    2 - Realizar uma operação sem retornar resultado; */
     /*Métodos possuem gategorias
     1 - Métodos de instância: Só podem ser usados, através de um objeto;
     2 - Métodos estáticos: Existem o escopo da classe, ou seja, não dependem de instâncias para serem usados*/
@@ -19,12 +19,22 @@ var Pessoa = /** @class */ (function () {
             return 'bueee... buee.... bueee....';
         }
         /*Implementação para criança de 1 a 10 anos*/
+        if (this.Idade >= 1 && this.Idade < 10) {
+            return 'O que é isso papai? O que é isso mamãe?';
+        }
+        return 'Não falarei nada';
     };
     Pessoa.prototype.Envelhecer = function (anosDeVida) {
         this.Idade = this.Idade + anosDeVida;
     };
     return Pessoa;
 }());
+//  Início do Ciclo da Vida 
 var grazi = Pessoa.Nascimento('Grazi', 0);
 console.log(grazi);
+console.log(grazi.Fala());
+var i;
+for (i = 0; i < 9; i++) {
+    grazi.Envelhecer(1);
+}
 console.log(grazi.Fala());
